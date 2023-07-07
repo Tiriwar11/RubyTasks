868a2c15c868251da17165b26c5126020e4b3cfe
Tiriwar11 <yefersonsalazar@itm.edu.co>
2023-07-06 23:27:00 -0500

    RubyStiwar

---
+++ Condicionals/Dowhile.rb
@@ -0,0 +1,16 @@
+# Ruby program to illustrate 'do..while'loop
+
+# starting of do..while loop
+loop do
+
+puts "GeeksforGeeks"
+
+val = '7'
+
+# using boolean expressions
+if val == '7'
+break
+end
+
+# ending of ruby do..while loop
+end
---
+++ Condicionals/Until.rb
@@ -0,0 +1,14 @@
+# Ruby program to illustrate 'until' loop
+
+var = 7
+
+# using until loop
+# here do is optional
+until var == 11 do
+
+# code to be executed
+puts var * 10
+var = var + 1
+
+# here loop ends
+end

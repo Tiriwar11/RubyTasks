b2df4c627149bbed38a4aecc78217e43cfb9ca41
Tiriwar11 <yefersonsalazar@itm.edu.co>
2023-07-06 23:23:08 -0500

    RubyStiwar

---
+++ Condicionals/While.rb
@@ -0,0 +1,15 @@
+# Ruby program to illustrate 'while' loop
+
+# variable x
+x = 4
+
+# using while loop
+
+while x >= 1
+
+# statements to be executed
+puts "GeeksforGeeks"
+x = x - 1
+
+# while loop ends here
+end
